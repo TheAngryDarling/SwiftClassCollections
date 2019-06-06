16ffbd5f98c6be5f82fdb9ce8382e1eea713d11d
Tyler Anger <tyler@tylers-macbook-pro.local>
2019-06-06 02:42:43 -0400

    Added property comment blocks for use with Xcode build in documentation Made SAnyDictionary inherit from SAnyArray

--- Sources/SwiftClassCollections/SAnyDictionary.swift
+++ Sources/SwiftClassCollections/SAnyDictionary.swift
@@ -7,12 +7,16 @@
 
 import Foundation
 
-public protocol SAnyDictionary {
+/// Protocol that defines any Dictionary
+public protocol SAnyDictionary: SAnyArray {
+    /// The Key Type constraint on this dictionary
     var keyType: Any.Type { get }
+    /// The Value Type constraint on this dictionary
     var valueType: Any.Type { get }
     
-    var count: Int { get }
-    var isEmpty: Bool { get }
-    
+    /// Accesses the value associated with the given key for reading.
+    ///
+    /// - Parameter key: The key to find in the dictionary.
+    /// - Returns: The value associated with key if key is in the dictionary; otherwise, nil.
     func item(forKey key: Any) -> Any?
 }
